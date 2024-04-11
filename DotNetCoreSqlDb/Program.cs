c75714ad944894a616e5b7c7ebf42ac9179401b8
Taimur Malik <malikt@gmail.com>
2024-04-11 11:04:31 -0500

    Configure DB & add migration bundle

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add database context and cache
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Azure_SQL_ConnectionString")));
 builder.Services.AddDistributedMemoryCache();
 
 // Add services to the container.
